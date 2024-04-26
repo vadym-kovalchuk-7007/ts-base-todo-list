39db6e6b1db33f779bfdadea34125fc4ff5fda12
TeeZ0NE <106161570+vadym-kovalchuk-deltatre@users.noreply.github.com>
2024-04-26 18:27:35 +0300

    Cr simple todo with bubl

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,52 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+import { v4 as uuid } from 'uuid';
 
-import confetti from 'canvas-confetti';
+const list = <HTMLUListElement>document.getElementById('list');
+const form = <HTMLFormElement>document.getElementById('task-form');
+const input = <HTMLInputElement>document.getElementById('task-title');
 
-confetti.create(document.getElementById('canvas') as HTMLCanvasElement, {
-  resize: true,
-  useWorker: true,
-})({ particleCount: 200, spread: 200 });
+interface Task {
+  completed: boolean;
+  createdAt: Date;
+  id: string;
+  title: string;
+}
+
+form?.addEventListener('submit', (e) => {
+  e.preventDefault();
+  if (!input?.value) return;
+
+  const newTask: Task = {
+    completed: false,
+    createdAt: new Date(),
+    id: uuid(),
+    title: input.value,
+  };
+  addItemToList(newTask);
+  input.value = '';
+});
+
+const addItemToList = (task: Task) => {
+  const item = document.createElement('li');
+  const label = document.createElement('label');
+  label.dataset.uid = task.id;
+  const checkBox = document.createElement('input');
+  checkBox.type = 'checkbox';
+  label.append(checkBox, task.title);
+  item.append(label);
+  list?.append(item);
+};
+
+list.addEventListener(
+  'click',
+  (evt) => {
+    const target = evt.target as HTMLInputElement;
+    const targetType = target.type;
+    if (targetType === 'checkbox') {
+      const label = target.closest('label');
+      if (label) {
+        label.style.textDecoration = target.checked ? 'line-through' : '';
+      }
+    }
+  },
+  false,
+);
